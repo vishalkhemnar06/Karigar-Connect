57752e85a41b795267044254db0e7e33352d2133
Gagan Dhanapune <135752329+gagan8605@users.noreply.github.com>
2026-05-02 09:11:24 +0530

    Delete server/models/analyticsRoutes.js

--- server/models/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getVisitorCount, recordUniqueVisitor } = require('../controllers/analyticsController');
-
-const router = express.Router();
-
-router.get('/visitors', getVisitorCount);
-router.post('/visitors', recordUniqueVisitor);
-
-module.exports = router;
